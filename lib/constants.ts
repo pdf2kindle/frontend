cf0fd74c56f7bf5780ca102188fedc588ac3d39c
vjsai <vijaysaijnv@gmail.com>
2024-12-14 19:23:20 +0530

    added azw3

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,12 +1,13 @@
 export const FILE_CONSTRAINTS = {
-  ALLOWED_EXTENSIONS: [".epub", ".mobi", ".pdf", ".djvu", ".pub"],
+  ALLOWED_EXTENSIONS: [".epub", ".mobi", ".pdf", ".djvu", ".pub",".azw3"],
   MAX_FILE_SIZE: 5 * 1024 * 1024, // 5MB
   MIME_TYPES: {
     'application/pdf': ['.pdf'],
     'application/epub+zip': ['.epub'],
     'application/x-mobipocket-ebook': ['.mobi'],
     'image/vnd.djvu': ['.djvu'],
-    'application/x-mspublisher': ['.pub']
+    'application/x-mspublisher': ['.pub'],
+    'application/vnd.amazon.mobi8-ebook': ['.azw3']
   }
 } as const;
 
